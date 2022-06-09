7b2be489b9d58e8c8c0fd33221ad2123220e2c49
Andres Villazon <andres.villazon@ceiba.com.co>
2022-06-09 10:35:18 -0500

    fix bugs sonar

--- src/app/core/mocks/auth-data-mock.ts
+++ src/app/core/mocks/auth-data-mock.ts
@@ -4,7 +4,6 @@ export const loginSuccesfullyResult = () => {
       id: 1,
       name: "Andres Villazon",
       email: "andres.villazon@ceiba.com",
-      password: "ceibaSoftware*123",
       token:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZHJlcy52aWxsYXpvbkBjZWliYS5jb20uY28iLCJpZCI6IjEiLCJpYXQiOjE1MTYyMzkwMjJ9.PU9kIdBC_9CGttcUGe5BpGHKD75Sxfdbr495ZevNQ4s",
     },
