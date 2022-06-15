0d8d0fc456d8a298bb5ee0441719f39e639a9a0a
Andres Villazon <andres.villazon@ceiba.com.co>
2022-06-15 14:51:10 -0500

    fix sonar

--- src/app/feature/parking/shared/model/create-parking-data.ts
+++ src/app/feature/parking/shared/model/create-parking-data.ts
@@ -6,21 +6,4 @@ export class CreateParking {
   basePrice: number;
   dominical: number;
   onDemand: number;
-
-  base(hours: number) {
-    return this.basePrice * hours ?? 0;
-  }
-  extraOnDemand(hours: number) {
-    return this.onDemand * hours ?? 0;
-  }
-
-  extraWeekend(hours: number) {
-    return this.dominical * hours ?? 0;
-  }
-
-  totalPrice(hours) {
-    return (
-      this.basePrice + this.extraOnDemand(hours) + this.extraWeekend(hours)
-    );
-  }
 }
